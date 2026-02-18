9047d2f78858b5224f0ebc866960d32e827120ec
Lucas-Fabri <dev.cunhaww@gmail.com>
2026-02-18 13:36:26 -0300

    feat: implement error handling middleware and import routes in server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,39 +6,73 @@
 4) por fim, ligamos o servidor na porta
  */
 import express from 'express';
+import { routes } from './routes/index'; // Importando as rotas do arquivo index.ts
+// import { myMiddleware } from './middlewares/my-middleware';
+import type { NextFunction, Request, Response } from 'express';
+import { AppError } from './utils/app-error';
+import { ZodError } from 'zod';
 
 const PORT = 3333; // Definindo a porta para o servidor
 const app = express(); // Inicializando o express
 app.use(express.json()); // Dizendo que vamos usar o JSON para representar dados no body da requisição
+// app.use(myMiddleware); // Usando o middleware em todas as rotas (Global, deve ser aplicado antes das rotas)
 
-// Recuperando um único parametro dinamico na rota
-app.get('/products/:id', (request, response) => {
-  // Express lida com facilidade com rotas
-  const { id } = request.params; // Pegando o id do produto pelo parametro da rota (req.params)
+app.use(routes); // Usando as rotas do arquivo index.ts
 
-  response.send(`Produto ${id}`);
-});
+// SEMPRE antes de listen fizemos o tratamento da exceção, ela vai no final de tudo para ser capaz de capturar as exceções que podem ocorrer nas rotas, middlewares etc
 
-// Recuperando parametros dinamicos na rota
-app.get('/products/:id/:user', (request, response) => {
-  // Express lida com facilidade com rotas
-  const { id, user } = request.params; // Pegando o id e o user do produto pelo parametro da rota (req.params)
+// 400 Bad request - Client
+// 500 Internaç server error - Server
 
-  response.send(`Produto ${id} do usuario ${user}`);
-});
+app.use(
+  (error: any, request: Request, response: Response, next: NextFunction) => {
+    // Verifica se o erro (error) é instancia/foi gerado pela classe AppError
+    // se for um erro de AppError então é um erro lançado por nós, enviamos para o cliente uma mensagem mais detalhada, porém quando não é erro de AppError então ´não fomos nós que lançamos portanto é um erro do servidor.
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({ message: error.message });
+    }
 
-// Recuperando parametros não nomeados Query params
-app.get('/products', (request, response) => {
-  const { page, limit } = request.query; // Pegando os query params da rota (req.query)
-  response.send(`Página ${page} de ${limit}`); //http://localhost:3333/products?page=1&limit=10
-});
+    if (error instanceof ZodError) {
+      response
+        .status(400)
+        .json({ message: 'Valdation error!', issues: error.format() });
+    }
 
-// Enviar dados com o body da requisição
-app.post('/products', (request, response) => {
-  const { name, price } = request.body; // Pegando os dados do body da requisição (req.body)
-  // response.send(`Produto ${name} custa $ ${price}`); // content-type text/plain
-  response.status(201).json({ name, price }); // Enviando os dados de volta como JSON Garante o content-type application/json e converte o objeto para JSON automaticamente
-});
+    response.status(500).json({ message: error.message });
+  },
+);
+
+// // Recuperando um único parametro dinamico na rota
+// app.get('/products/:id', (request, response) => {
+//   // Express lida com facilidade com rotas
+//   const { id } = request.params; // Pegando o id do produto pelo parametro da rota (req.params)
+
+//   response.send(`Produto ${id}`);
+// });
+
+// // Recuperando parametros dinamicos na rota
+// app.get('/products/:id/:user', (request, response) => {
+//   // Express lida com facilidade com rotas
+//   const { id, user } = request.params; // Pegando o id e o user do produto pelo parametro da rota (req.params)
+
+//   response.send(`Produto ${id} do usuario ${user}`);
+// });
+
+// // Recuperando parametros não nomeados Query params
+// app.get('/products', (request, response) => {
+//   const { page, limit } = request.query; // Pegando os query params da rota (req.query)
+//   response.send(`Página ${page} de ${limit}`); //http://localhost:3333/products?page=1&limit=10
+// });
+
+// // Enviar dados com o body da requisição
+// // Middleware local em uma rota específica, ou seja, o middleware só vai ser executado quando a rota POST /products for chamada
+// app.post('/products', myMiddleware, (request, response) => {
+//   // o next de dentro do middleware sabe qual é a próxima função, ou seja, a função de callback da rota POST /products
+//   const { name, price } = request.body; // Pegando os dados do body da requisição (req.body)
+//   // response.send(`Produto ${name} custa $ ${price}`); // content-type text/plain
+//   response.status(201).json({ name, price, user_id: request.user_id }); // Enviando os dados de volta como JSON Garante o content-type application/json e converte o objeto para JSON automaticamente
+//   // user_id é o tipo que adicionamos no arquivo .d.ts, e que foi adicionado ao objeto request dentro do middleware, ou seja, ele pode ser acessado em qualquer rota que use esse middleware
+// });
 
 app.listen(PORT, () => {
   // Porta com funcao de callback
